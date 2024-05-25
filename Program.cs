66af24ff6549d818792a776f7f2d690805315be9
Ricardo Souza <ricksouz96@gmail.com>
2024-05-25 19:46:13 -0300

    Adiciona testes de execução da classe Iphone

--- Program.cs
+++ Program.cs
@@ -5,3 +5,10 @@ Console.WriteLine("Smartphone Nokia:");
 Smartphone nokia = new Nokia("123456", "Modelo 1", "123456", 4);
 nokia.Ligar();
 nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("987654", "Modelo 2", "987654", 8);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
