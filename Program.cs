7bfaf42254d9f1421de3fb7d9f645a5a48495348
Ricardo Souza <ricksouz96@gmail.com>
2024-05-25 19:40:00 -0300

    Adiciona testes para verificar a execução da classe Nokia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "123456", 4);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
